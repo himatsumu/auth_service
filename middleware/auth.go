6b96308b354b5863e9518f9d94aa245534e5c644
toma1128 <kirima--idea--@au.com>
2025-09-04 02:46:23 +0900

    [fix]一時ドメイン直打ち

--- middleware/auth.go
+++ middleware/auth.go
@@ -28,7 +28,7 @@ func CORSMiddleware(next http.Handler) http.Handler {
 		// if isAllowed {
 		// 	w.Header().Set("Access-Control-Allow-Origin", origin)
 		// }
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "himatsumu.kirimaru.org")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
