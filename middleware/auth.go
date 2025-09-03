553a2e077b34851db2b37b434df0ed8a78528c11
toma1128 <kirima--idea--@au.com>
2025-09-04 02:17:32 +0900

    [update]複数のオリジンを許可するように変更

--- middleware/auth.go
+++ middleware/auth.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"net/http"
 	"os"
+	"strings"
 )
 
 // CORSMiddleware はCORSを設定する
@@ -10,7 +11,23 @@ func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		reactUrl := os.Getenv("REACT_URL")
 
-		w.Header().Set("Access-Control-Allow-Origin", reactUrl)
+		// 許可したいオリジンのリスト
+		allowlist := []string{reactUrl, "https://himatsumu.kirimaru.org"}
+		
+		origin := r.Header.Get("Origin")
+		
+		// リクエストのOriginが許可リストに含まれているかを確認
+		isAllowed := false
+		for _, allowedOrigin := range allowlist {
+			if strings.EqualFold(origin, allowedOrigin) {
+				isAllowed = true
+				break
+			}
+		}
+
+		if isAllowed {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
